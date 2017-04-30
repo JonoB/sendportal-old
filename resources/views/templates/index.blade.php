0cc60c5018ba0ad22cee2641a0404256cd9f3c60
Jono <jonathan.behr@gmail.com>
2017-04-30 22:58:45 +0100

    Cols

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -12,7 +12,7 @@
 
     <div class="row">
         @foreach($templates as $template)
-            <div class="col-sm-3 col-xs-6 template-item">
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-6 template-item">
                 <div class="pull-left">
                     <h4>{{ $template->name }}</h4>
                 </div>
