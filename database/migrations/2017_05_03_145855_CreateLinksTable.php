b5ee7de36cc60e6a6c24ea6e81786742fb7ef40d
JonoB <jonathan.behr@gmail.com>
2017-05-04 20:31:09 +0100

    Add links table

---
+++ database/migrations/2017_05_03_145855_CreateLinksTable.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('links', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('newsletter_id');
+            $table->string('original_link');
+            $table->string('tracking_link');
+            $table->string('counter');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
+
