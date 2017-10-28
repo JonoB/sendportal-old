6b5c8e6f9b58f7a3259328ca8763de04885cfa35
JonoB <jonathan.behr@gmail.com>
2017-10-28 15:16:54 +0100

    Start work on api

---
+++ app/Http/Controllers/Api/ApiSubscribersController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\SubscriberRequest;
+use App\Interfaces\SubscriberRepositoryInterface;
+use App\Interfaces\TagRepositoryInterface;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class ApiSubscribersController extends Controller
+{
+    /**
+     * @var SubscriberRepositoryInterface
+     */
+    protected $subscriberRepository;
+
+    /**
+     * @var TagRepositoryInterface
+     */
+    protected $tagRepository;
+
+    /**
+     * SubscribersController constructor.
+     *
+     * @param SubscriberRepositoryInterface $subscriberRepository
+     */
+    public function __construct(
+        SubscriberRepositoryInterface $subscriberRepository,
+        TagRepositoryInterface $tagRepository
+    )
+    {
+        $this->subscriberRepository = $subscriberRepository;
+        $this->tagRepository = $tagRepository;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $subscribers = $this->subscriberRepository->paginate('email', ['tags']);
+
+        return view('subscribers.index', compact('subscribers'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data = [
+            'tags' => $this->tagRepository->all(),
+            'selectedTags' => [],
+        ];
+
+        return view('subscribers.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  SubscriberRequest $request
+     * @return RedirectResponse
+     */
+    public function store(SubscriberRequest $request)
+    {
+        $subscriber = $this->subscriberRepository->store($request->all());
+        $this->subscriberRepository->syncTags($subscriber, $request->get('tags'));
+
+        return redirect()->route('subscribers.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('subscribers.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $subscriber = $this->subscriberRepository->find($id, ['tags']);
+
+        $data = [
+            'subscriber' => $subscriber,
+            'tags' => $this->tagRepository->all(),
+            'selectedTags' => selectedOptions('tags', $subscriber),
+        ];
+
+        return view('subscribers.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param SubscriberRequest $request
+     * @param int $id
+     * @return RedirectResponse
+     */
+    public function update(SubscriberRequest $request, $id)
+    {
+        $subscriber = $this->subscriberRepository->update($id, $request->all());
+        $this->subscriberRepository->syncTags($subscriber, $request->get('tags'));
+
+        return redirect()->route('subscribers.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
